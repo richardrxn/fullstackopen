1aa8ad022a744767506e58ef201e23fecc93cb44
rullebrett <74204667+rullebrett@users.noreply.github.com>
2025-09-21 23:14:58 -0400

    Exercise 1.2 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,30 +10,36 @@ const App = () => {
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
 
 const Header = (props) => {
-  console.log(props)
   return (
   <h1>{props.course}</h1>
   )
 }
 
 const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.part1} exercises={props.exercises1}/>
+      <Part name={props.part2} exercises={props.exercises2}/>
+      <Part name={props.part3} exercises={props.exercises3}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
   console.log(props)
   return (
-    <p>{props.part} {props.exercises}</p>
+    <p>{props.name} {props.exercises}</p>
   )
 }
 
 const Total = (props) => {
-  console.log(props)
   return (
     <p>Number of exercises {props.total}</p>
   )
