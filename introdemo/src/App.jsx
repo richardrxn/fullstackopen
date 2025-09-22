9250e9949e5ff0e3ddf4cd4c93bc64efa7919c28
rullebrett <74204667+rullebrett@users.noreply.github.com>
2025-09-21 22:03:11 -0400

    Learning react with introdemo

---
+++ introdemo/src/App.jsx
@@ -0,0 +1,32 @@
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/richardrxn'>richardrxn</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={25+10} />
+      <Hello name={name} age={age}/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App; Hello
